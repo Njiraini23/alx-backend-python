2b731d207c13a54c8e01a6fef72d132915aaec52
Njiraini23 <njiraini88@gmail.com>
2023-08-16 18:20:23 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Basic annotations to string"""
+
+
+def to_str(n: float) -> str:
+    '''A type annotation that takes a float n 
+    and returns the string representation'''
+    return str(n)
