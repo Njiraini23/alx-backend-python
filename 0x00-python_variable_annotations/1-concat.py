847e7c91071603bdd04b73e2881d97840559eed0
Njiraini23 <njiraini88@gmail.com>
2023-08-16 16:49:38 +0000

    1-main.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""A concat that takes string str1 and str2"""
+def concat(str1: str, str2: str) -> str:
+    '''A function that adds two strings together'''
+    return str1 + str2
