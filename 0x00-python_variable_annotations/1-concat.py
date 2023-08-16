cf1d4307946f12ffb7aa2e553f0f9f0ea63b6ea0
Njiraini23 <njiraini88@gmail.com>
2023-08-16 16:50:47 +0000

    pycodestyle fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """A concat that takes string str1 and str2"""
+
+
 def concat(str1: str, str2: str) -> str:
     '''A function that adds two strings together'''
     return str1 + str2
