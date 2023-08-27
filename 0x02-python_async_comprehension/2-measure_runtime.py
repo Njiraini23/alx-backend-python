a39bba23d63674a072e7dc19cdd3cb905c3a0b0a
Njiraini23 <njiraini88@gmail.com>
2023-08-27 15:36:59 +0000

    2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""measure runtime coroutine that executes 4 times"""
+import asyncio
+import time
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """measures the total runtime"""
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = asyncio.get_event_loop().time()
+    return end_time - start_time
