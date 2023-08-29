02d8b6a5065487e7cb62620fba8d79a7a4a2e72c
Njiraini23 <njiraini88@gmail.com>
2023-08-29 09:23:29 +0000

    0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-"""takes an integer argument with default value of 10"""
+"""asynchronous coroutine"""
+import asyncio
+import random
+
+
 async def wait_random(max_delay: int = 10) -> float:
-    """waits for random delay"""
+    """return wait time"""
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
     return wait_time
