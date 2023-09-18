e12d3a6d0302a5eaf87956817dc1b9e10355d30d
Njiraini23 <njiraini88@gmail.com>
2023-09-18 13:52:39 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+"""Unit test for utils.access_nested_map"""
+import unnittest
+from typing import Dict, Tuple, Union
+from parameterized import parameterized
+from utils import acccess_nested_map, get_json
+from unittetest.mock import patch, Mock
+
+
+class TestAccessNestedMap(unnittest.TestCase):
+    """TestAccessNestedMap class that inherits"""
+    @parameterized.expand([
+        ("a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+
+    def test_access_nested_map(
+            self,
+            nested_map
+            path:
+            expected_result: Union[Dict, int],
+            ) -> None:
+        """The tests for access nested map"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception_msg: Exception,
+            ) -> None:
+        """Tests for all exceptions"""
+        with self.assertRaise(exception_msg):
+            access_nested_map(nested_map, path)
