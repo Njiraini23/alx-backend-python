eb8557f9de1fe2fea7292aeb79799dbd39815465
Njiraini23 <njiraini88@gmail.com>
2023-09-19 04:25:20 +0000

    utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Unit test for utils.access_nested_map"""
+
 import unnittest
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
@@ -36,3 +37,40 @@ class TestAccessNestedMap(unittest.TestCase):
         """Testing for exceptions"""
         with self.assertRaises(exception_msg):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Get Json class to test utils"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    def test_get_json(self: Any, test_url: str, test_payload: Dict) -> None:
+        """This a test for the json file"""
+        attributes = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attribute)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test for the memoize function"""
+    def test_memoize(self) -> None:
+        """Testing the memoizing function"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+    with patch.object(
+            TestClass,
+            "a_method",
+            return_value=lambda: 42,
+            ) as memo_fxn:
+        test_class = TestClass()
+        self.assertEqual(test_class.a_property(), 42)
+        self.assertEqual(test_class.a_property(), 42)
+        memo_fxn.assert_called_once()
